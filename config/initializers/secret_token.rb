73f2fdbe130bcfc6c0d9d38ceacc2054fedd6dd6
Nimar Bhai <nimarbhai@gmail.com>
2014-02-24 11:37:37 +0530

    initial app push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kisanbhai::Application.config.secret_token = '8158eb1e5c818bfbdf50fc0f876e7de443fa26fbdb7cdccab1e3baefc8a49b2b418468f5226a64f96379e2b2ffa771ad484fd6cde7c07db009cd40aa14fcfd23'
